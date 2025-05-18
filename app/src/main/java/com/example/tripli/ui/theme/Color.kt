70360b181efaf45bcc0291b45a5013380d68519b
Fiiliip <50334258+Fiiliip@users.noreply.github.com>
2025-05-18 22:08:55 +0200

    Updated theme to custom

---
+++ app/src/main/java/com/example/tripli/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.example.tripli.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFFF5AC9C)
+val Secondary = Color(0xFF545773)
+val Tertiary = Color(0xFFE95F68)
+
+val Success = Color(0xFF00C851)
+val Warning = Color(0xFFFFBB33)
+val Danger = Color(0xFFFF4444)
+
+val Light = Color(0xFFE3E3E3)
+val Medium = Color(0xFFB3B3B3)
+val Dark = Color(0xFF2F2F2F)
